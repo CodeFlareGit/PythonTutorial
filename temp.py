347affb06215c2d920633f0856d198edd17500d5
zach <codeflarecontact@gmail.com>
2021-11-24 14:54:22 -0600

    initial commit

---
+++ temp.py
@@ -0,0 +1,23 @@
+populations = [1000, 2000, 3000, 4000, 5000, 6000, 2000]
+
+print(populations)
+
+print(populations[-1])
+
+print(type(populations[-1]))
+
+print(populations[0])
+
+print(populations[1])
+
+print(populations[-2])
+print(populations[4])
+
+populations.append(7000)
+print(populations)
+
+populations.insert(3, 3500)
+print(populations)
+
+populations = list(filter((2000).__ne__, populations))
+print(populations)
