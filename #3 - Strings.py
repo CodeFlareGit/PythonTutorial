65a69e83292a0a6f8b61ec1c92e55a9939091a32
zach <codeflarecontact@gmail.com>
2021-11-24 16:34:23 -0600

    Updates

--- #3 - Strings.py
+++ #3 - Strings.py
@@ -4,6 +4,16 @@ print(type(Sentence))
 name = "Zach"
 print(Sentence + name)
 
-print('here')
+num1 = 5
+num2 = 5
 
-print('got here')
+print (num1 + num2)
+
+num3 = '5'
+num4 = '5'
+
+print (num3 + num4)
+
+print(len(Sentence))
+print(Sentence[3])
+print(Sentence[3:7])
