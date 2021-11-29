ce5758525e42f15b464fd9bc2fa45e178511dd94
zach <codeflarecontact@gmail.com>
2021-11-29 15:16:27 -0600

    my changes

---
+++ #2 Strings.py
@@ -0,0 +1,19 @@
+Sentence = 'My name is: '
+print(type(Sentence))
+
+Name = 'Zach'
+print(Sentence+Name)
+
+num1 = 5
+num2 = 5
+print(num1+num2)
+
+num1 = '5'
+num2 = '5'
+print(num1+num2)
+
+print(len(Sentence))
+print(Sentence[3])
+
+print(Sentence[3:7])
+
